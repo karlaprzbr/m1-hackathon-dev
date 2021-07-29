9eea3ed528cd974a676c56b229d2d59dd7459cc7
Karla Pérez <44835870+karlaprzbr@users.noreply.github.com>
2021-07-29 11:26:36 +0200

    add reaction and utilisateur components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactionComponent } from './reaction/reaction.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactionComponent,
+    UtilisateurComponent
   ],
   imports: [
     BrowserModule
